522f25760fe5fcbcf903653b53e7fb847732d397
marcosrogerio-jrf <marcosrogerio.jrf@gmail.com>
2024-07-26 17:15:53 -0400

    First Commit

---
+++ src/main/java/br/edu/ifms/Tads6TesteSoftwareApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tads6TesteSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tads6TesteSoftwareApplication.class, args);
+	}
+
+}
