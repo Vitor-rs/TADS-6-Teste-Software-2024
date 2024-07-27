26e43dd8583a6caef1d3d59cd27322948feba9b1
marcosrogerio-jrf <marcosrogerio.jrf@gmail.com>
2024-07-27 13:36:51 -0400

    preparacao para os testes

--- src/main/java/br/edu/ifms/Tads6TesteSoftwareApplication.java
+++ src/main/java/br/edu/ifms/Tads6TesteSoftwareApplication.java
@@ -2,9 +2,18 @@ package br.edu.ifms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Tads6TesteSoftwareApplication {
+@EnableWebMvc
+public class Tads6TesteSoftwareApplication implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tads6TesteSoftwareApplication.class, args);
