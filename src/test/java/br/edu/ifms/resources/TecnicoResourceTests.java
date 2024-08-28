a04cd6bb7d42ab08ecc4be6c7302a09b7885e0ce
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2024-08-27 20:57:38 -0400

    Alterações aula 27-08-2024: Testes para camada resource da classe TecnicoResource com a classe TecnicoResourceTests.

--- src/test/java/br/edu/ifms/resources/TecnicoResourceTests.java
+++ src/test/java/br/edu/ifms/resources/TecnicoResourceTests.java
@@ -2,6 +2,7 @@ package br.edu.ifms.resources;
 
 import br.edu.ifms.dto.TecnicoDTO;
 import br.edu.ifms.services.TecnicoService;
+import br.edu.ifms.services.exceptions.DataBaseException;
 import br.edu.ifms.services.exceptions.ResourceNotFoundException;
 import br.edu.ifms.tests.Factory;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,179 +14,170 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Classe de testes para o recurso TecnicoResource.
+ *
+ * Esta classe contém testes unitários que verificam o comportamento da classe
+ * TecnicoResource, que é responsável por gerenciar as operações relacionadas
+ * aos técnicos. Os testes são realizados utilizando o MockMvc para simular
+ * requisições HTTP e verificar as respostas da API.
+ *
+ * Os testes incluem:
+ * - Atualização de técnicos
+ * - Busca de técnicos por ID
+ * - Listagem paginada de técnicos
+ *
+ * Os testes utilizam mocks do serviço TecnicoService para isolar a lógica
+ * de controle da lógica de serviço, permitindo verificar se as respostas
+ * da API estão corretas.
+ *
+ * @see TecnicoResource
  */
 @WebMvcTest(TecnicoResource.class)
 public class TecnicoResourceTests {
 
-    // Injeta o MockMvc para simular requisições HTTP
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private TecnicoService service;
 
-    // Injeta o ObjectMapper para converter objetos em JSON
     @Autowired
     private ObjectMapper objectMapper;
 
-    private Long existingId;
-    private Long nonExistingId;
     private TecnicoDTO tecnicoDTO;
     private PageImpl<TecnicoDTO> page;
+    private Long idExistente;
+    private Long idInexistente;
+    private Long idDependente;
 
     /**
      * Configuração inicial dos testes.
      *
-     * @throws Exception se ocorrer algum erro durante a configuração
+     * Este método é executado antes de cada teste, configurando os dados
+     * necessários e as respostas esperadas do serviço mockado.
+     *
+     * @throws Exception se ocorrer um erro durante a configuração.
      */
     @BeforeEach
-    public void setUp() throws Exception {
-        existingId = 1L;
-        nonExistingId = 2L;
+    void setUp() throws Exception {
+        idExistente = 1L;
+        idInexistente = 100L;
+
         tecnicoDTO = Factory.createTecnicoDTO();
         page = new PageImpl<>(List.of(tecnicoDTO));
 
         when(service.findAllPaged(any())).thenReturn(page);
+        when(service.findById(idExistente)).thenReturn(tecnicoDTO);
+        when(service.findById(idInexistente)).thenThrow(ResourceNotFoundException.class);
 
-        when(service.findById(existingId)).thenReturn(tecnicoDTO);
-        when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+        // UPDATE
+        when(service.update(eq(idExistente), any())).thenReturn(tecnicoDTO);
+        when(service.update(eq(idInexistente), any())).thenThrow(ResourceNotFoundException.class);
 
+        // Insert
         when(service.insert(any())).thenReturn(tecnicoDTO);
 
-        when(service.update(eq(existingId), any())).thenReturn(tecnicoDTO);
-        when(service.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
-
-        doNothing().when(service).delete(existingId);
-        doThrow(ResourceNotFoundException.class).when(service).delete(nonExistingId);
+        // Delete
+        doNothing().when(service).delete(idExistente);
+        doThrow(ResourceNotFoundException.class).when(service).delete(idInexistente);
+        doThrow(DataBaseException.class).when(service).delete(idDependente);
     }
 
     /**
-     * Testa se o método findAllPaged retorna uma página de TecnicoDTO.
+     * Testa a atualização de um técnico existente.
      *
-     * @throws Exception se ocorrer algum erro durante a execução do teste
+     * Este teste verifica se a atualização de um técnico com um ID existente
+     * retorna um status 200 OK e se o corpo da resposta contém os dados
+     * do técnico atualizado.
+     *
+     * @throws Exception se ocorrer um erro durante a execução do teste.
      */
     @Test
-    public void findAllPagedDeveriaRetornarPagina() throws Exception {
-        mockMvc.perform(get("/tecnicos")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-    /**
-     * Testa se o método findById retorna TecnicoDTO quando o ID é existente.
-     *
-     * @throws Exception se ocorrer algum erro durante a execução do teste
-     */
-    @Test
-    public void findByIdDeveriaRetornarTecnicoQuandoIdExistir() throws Exception {
-        mockMvc.perform(get("/tecnicos/{id}", existingId)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.nome").exists());
-    }
-
-    /**
-     * Testa se o método findById retorna NotFound quando o ID não existe.
-     *
-     * @throws Exception se ocorrer algum erro durante a execução do teste
-     */
-    @Test
-    public void findByIdDeveriaRetornarNotFoundQuandoIdNaoExistir() throws Exception {
-        // O mockMvc não aceita exceções, então é necessário verificar o status da resposta
-        mockMvc.perform(get("/tecnicos/{id}", nonExistingId)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
-    }
-
-    /**
-     * Testa se o método insert retorna TecnicoDTO criado.
-     *
-     * @throws Exception se ocorrer algum erro durante a execução do teste
-     */
-    @Test
-    public void insertDeveriaRetornarTecnicoCriado() throws Exception {
-        // Converte o objeto em JSON
+    public void updateDeveriaRetornarTecnicoQuandoIdExistente() throws Exception {
         String jsonBody = objectMapper.writeValueAsString(tecnicoDTO);
-
-        // Faz a requisição HTTP
-        mockMvc.perform(post("/tecnicos")
-                        .content(jsonBody)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.nome").exists());
+        ResultActions result = mockMvc.perform(put("/tecnicos/{id}", idExistente)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").exists());
+        result.andExpect(jsonPath("$.nome").exists());
     }
 
     /**
-     * Testa se o método update retorna TecnicoDTO atualizado quando o ID é existente.
+     * Testa a atualização de um técnico com um ID inexistente.
      *
-     * @throws Exception se ocorrer algum erro durante a execução do teste
+     * Este teste verifica se a tentativa de atualização de um técnico com
+     * um ID que não existe retorna um status 404 Not Found.
+     *
+     * @throws Exception se ocorrer um erro durante a execução do teste.
      */
     @Test
-    public void updateDeveriaRetornarTecnicoQuandoIdExistir() throws Exception {
+    public void updateDeveriaRetornarNotFoundQuandoIdInexistente() throws Exception {
         String jsonBody = objectMapper.writeValueAsString(tecnicoDTO);
-
-        mockMvc.perform(put("/tecnicos/{id}", existingId)
-                        .content(jsonBody)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.nome").exists());
+        ResultActions result = mockMvc.perform(put("/tecnicos/{id}", idInexistente)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isNotFound());
     }
 
     /**
-     * Testa se o método update retorna NotFound quando o ID não existe.
+     * Testa a listagem paginada de técnicos.
      *
-     * @throws Exception se ocorrer algum erro durante a execução do teste
+     * Este teste verifica se a requisição para listar todos os técnicos
+     * retorna um status 200 OK.
+     *
+     * @throws Exception se ocorrer um erro durante a execução do teste.
      */
     @Test
-    public void updateDeveriaRetornarNotFoundQuandoIdNaoExistir() throws Exception {
-        String jsonBody = objectMapper.writeValueAsString(tecnicoDTO);
-
-        mockMvc.perform(put("/tecnicos/{id}", nonExistingId)
-                        .content(jsonBody)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
+    public void findAllPagedDeveriaRetornarPage() throws Exception {
+        ResultActions result = mockMvc.perform(get("/tecnicos")
+                .accept(MediaType.APPLICATION_JSON)
+        );
+        result.andExpect(status().isOk());
     }
 
     /**
-     * Testa se o método delete retorna NoContent quando o ID é existente.
+     * Testa a busca de um técnico por ID existente.
      *
-     * @throws Exception se ocorrer algum erro durante a execução do teste
+     * Este teste verifica se a busca de um técnico com um ID existente
+     * retorna um status 200 OK e se o corpo da resposta contém os dados
+     * do técnico.
+     *
+     * @throws Exception se ocorrer um erro durante a execução do teste.
      */
     @Test
-    public void deleteDeveriaRetornarNoContentQuandoIdExistir() throws Exception {
-        // O mockMvc não aceita métodos DELETE com corpo, então não é necessário enviar um JSON
-        mockMvc.perform(delete("/tecnicos/{id}", existingId)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNoContent());
+    public void findByIdDeveriaRetornarTecnicoQuandoIdExistente() throws Exception {
+        ResultActions result = mockMvc.perform(get("/tecnicos/{id}", idExistente).accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").exists());
     }
 
     /**
-     * Testa se o método delete retorna NotFound quando o ID não existe.
+     * Testa a busca de um técnico por ID inexistente.
      *
-     * @throws Exception se ocorrer algum erro durante a execução do teste
+     * Este teste verifica se a busca de um técnico com um ID que não existe
+     * retorna um status 404 Not Found.
+     *
+     * @throws Exception se ocorrer um erro durante a execução do teste.
      */
     @Test
-    public void deleteDeveriaRetornarNotFoundQuandoIdNaoExistir() throws Exception {
-        mockMvc.perform(delete("/tecnicos/{id}", nonExistingId)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
+    public void findByIdDeveriaRetornarExceptionNotFoundQuandoIdInexistente() throws Exception {
+        ResultActions result = mockMvc.perform(get("/tecnicos/{id}", idInexistente).accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isNotFound());
     }
 }
